4a7105fbb0cde251e829766827e24f8b35fba8f9
nospaceship <68411297+nospaceship@users.noreply.github.com>
2025-02-19 14:20:24 -0500

    Update update_motd.sh

--- update_motd.sh
+++ update_motd.sh
@@ -50,22 +50,32 @@ MEMORY_USAGE=$(free -h | awk '/^Mem:/ {print $3 "/" $2}')
 DISK_USAGE=$(df -h / | awk 'NR==2 {print $3 "/" $2}')
 PING_RESULT=$(ping -c 1 8.8.8.8 &> /dev/null && echo "Online" || echo "Offline")
 
+last_login_info=$(last -n 2 -R | tail -n 1)
+last_login_user=$(echo "$last_login_info" | awk '{print $1}')
+last_login_ip=$(echo "$last_login_info" | awk '{print $3}')
+last_login_time=$(echo "$last_login_info" | awk '{print $4, $5, $6, $7, $8}')
+last_login_details="User: $last_login_user, IP: $last_login_ip, Time: $last_login_time"
+
 # Create the new MOTD content
 NEW_CONTENT=$(cat << EOF
 
 $(figlet "JORDAN'S LAB")
 
 
-Welcome to my Homelab!
+Welcome to my Homelab! This is private lab, all activity is being monitored 
+and logged. Please be mindful of the best security practices.
 
 System Information:
--------------------
+--------------------------------
 IP Address       : $IP_ADDRESS
 Hostname         : $HOSTNAME
 CPU Temperature  : $CPU_TEMP°C
 Memory Usage     : $MEMORY_USAGE
 Disk Usage       : $DISK_USAGE
 Internet Status  : $PING_RESULT
+Last Login       : $last_login_details
+
+
 
 EOF
 )
@@ -74,7 +84,6 @@ EOF
 sudo cp $MOTD_FILE ${MOTD_FILE}.bak
 
 # Remove existing '#' border lines and append new content
-sudo sed -i '/^#.*$/d' $MOTD_FILE
 echo "$NEW_CONTENT" | sudo tee -a $MOTD_FILE > /dev/null
 
 # Apply correct permissions
